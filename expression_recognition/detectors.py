186ffe24cad68440f9246a67365d2e3f7dfff114
Alexey Krikunov <alexey.v.krikunov@yandex.ru>
2018-07-16 21:25:11 +0300

    Open mouth detector added

--- expression_recognition/detectors.py
+++ expression_recognition/detectors.py
@@ -4,7 +4,7 @@ from sklearn.externals import joblib
 from warnings import warn
 from pkg_resources import resource_filename
 
-from .feature_utils import flatten, to_dists_dataset
+from .feature_utils import flatten, to_dists_dataset, get_mouthes_only
 
 try:
     mo_model_file = resource_filename(__name__, "models/mouth_opened_model.sav")
@@ -21,9 +21,12 @@ except:
             Please prepare models using "experiments/models_selection.ipynb" and rebuild package""")
 
 
-def is_mouth_opened(landmarks):
-    features = flatten(landmarks)
-    prediction = mouth_opened_model.predict(features)
+def is_mouth_opened(landmarks, th=.2):
+    mouthes = get_mouthes_only(landmarks)
+    features = flatten(mouthes)
+
+    probsbilities = mouth_opened_model.predict_proba(features)[:,1]
+    prediction = [1 if p>=th else 0 for p in probsbilities]
     return prediction
 
 def is_smiling(landmarks):
